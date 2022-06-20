ac7122c793eb9527c3499d9590d6bff6438cf748
Artem Kiselev <kuzuru.dev@gmail.com>
2022-06-20 16:22:29 +0500

    develop(config/init): Added Init() and Runtime() methods

---
+++ config/init.go
@@ -0,0 +1,26 @@
+package config
+
+import (
+	"runtime"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/spf13/viper"
+
+	"main/internal/log"
+)
+
+func Init() error {
+	viper.AddConfigPath("./config")
+	viper.SetConfigName("config")
+
+	return viper.ReadInConfig()
+}
+
+// Runtime sets the number of operating system threads
+func Runtime() {
+	NumCPU := runtime.GOMAXPROCS(runtime.NumCPU())
+
+	if !fiber.IsChild() {
+		log.Info.Printf("Running with %d CPUs\n", NumCPU)
+	}
+}
