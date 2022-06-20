24d813d25c8e0cde0acd3e7edec88fff503e4dfe
Artem Kiselev <kuzuru.dev@gmail.com>
2022-06-20 16:33:00 +0500

    develop(internal/runners): Implemented startup logic for gRPC Server

---
+++ internal/runners/grpcserver.go
@@ -0,0 +1,42 @@
+package runners
+
+import (
+	"google.golang.org/grpc"
+	"net"
+
+	"github.com/getsentry/sentry-go"
+	"github.com/spf13/viper"
+
+	"auth-service/config"
+	"auth-service/internal/handlers"
+	"auth-service/internal/log"
+	ValidateService "auth-service/pkg/JWTValidationService"
+)
+
+func StartGRPC() {
+	// Start gRPC server
+	log.Info.Printf("Starting gRPC server on port: %s\n", "localhost:4000")
+
+	if err := config.Init(); err != nil {
+		sentry.CaptureException(err)
+		log.Error.Fatalf("%s", err.Error())
+	}
+
+	// Starting gRPC server
+	lis, err := net.Listen("tcp", "localhost:"+viper.GetString("ports.grpc"))
+	if err != nil {
+		sentry.CaptureException(err)
+		log.Error.Fatal(err)
+	}
+
+	// Creating new gRPC server handler
+	s := grpc.NewServer()
+	gRPCServer := &handlers.GRPCServer{}
+
+	ValidateService.RegisterJWTValidationServiceServer(s, gRPCServer)
+
+	if err := s.Serve(lis); err != nil {
+		sentry.CaptureException(err)
+		log.Error.Fatal(err)
+	}
+}
