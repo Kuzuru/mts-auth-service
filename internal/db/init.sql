3add84c47c62ea36896a02b528a836749a81422b
Artem Kiselev <kuzuru.dev@gmail.com>
2022-07-17 13:28:54 +0500

    reformat: Reformatted files

--- internal/db/init.sql
+++ internal/db/init.sql
@@ -1,9 +1,10 @@
-create table users(
-                      id  serial
-                          constraint users_pk
-                              primary key,
-                      login    varchar(40)  not null,
-                      password varchar(256) not null
+create table users
+(
+    id       serial
+        constraint users_pk
+            primary key,
+    login    varchar(40)  not null,
+    password varchar(256) not null
 );
 
 create unique index users_id_uindex
